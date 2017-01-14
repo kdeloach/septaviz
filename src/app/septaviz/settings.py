8c6e0d2ede2140be619c4af78f1de3ec38f38e1f
Kevin DeLoach <kdeloach@gmail.com>
2017-01-14 18:33:58 -0500

    Create DJANGO_ALLOWED_HOSTS environment variable

--- src/app/septaviz/settings.py
+++ src/app/septaviz/settings.py
@@ -24,10 +24,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '192.167.1.2',
-]
+ALLOWED_HOSTS = os.environ['DJANGO_ALLOWED_HOSTS'].split(',')
 
 
 # Application definition
